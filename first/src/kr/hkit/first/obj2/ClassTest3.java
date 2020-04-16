b490fa7e8bd2ea2e942dd7b46692552e099c2510
doheum <doheum@192.168.219.102>
2020-04-16 11:19:31 +0900

    다형성 설명중(3)

--- first/src/kr/hkit/first/obj2/ClassTest3.java
+++ first/src/kr/hkit/first/obj2/ClassTest3.java
@@ -3,20 +3,32 @@ package kr.hkit.first.obj2;
 public class ClassTest3 {
 
 	public static void main(String[] args) {
+		
 		ClassC cc = new ClassC();
 		callMethod(cc);
+		
+		ClassB cb = cc;
 		/*
 		callMethod(new ClassB());
 		
-		callMethod(new ClassA());*/
+		callMethod(new ClassA());
+		*/
 	}
 	
 	public static void callMethod(ClassA ca) {
 		ca.methodA();
 		
+		if(ca instanceof ClassB) {
+			ClassB cb = (ClassB)ca;
+			cb.methodB();
+		} 
+		if(ca instanceof ClassC) {
+			ClassC cc = (ClassC)ca;
+			cc.methodC();
+		}
+		
 		//ca에 들어온 객체가 ClassB였다면
 		//추가로 methodB 메소드를 호출해 주세요.
-		
 		//ca에 들어온 객체가 ClassC였다면
 		//추가로 methodB, methodC 메소드를 호출해 주세요
 	}
